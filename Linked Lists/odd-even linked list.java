f8f415ed1d3dc0608f104ffe72e3cb38508e65e4
Bhavya Aggarwal <87543207+Aggarwal-Bhavya@users.noreply.github.com>
2022-12-07 00:45:57 +0530

    Daily Challenge

---
+++ Linked Lists/odd-even linked list.java
@@ -0,0 +1,20 @@
+//Leetcode Q-328
+//Odd-Even Linked List
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null)
+            return head;
+        ListNode odd = head, even = head.next, evenHead = even;
+
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+//TC = O(N) and SC = O(1)
